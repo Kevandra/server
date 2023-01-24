20d8d6158e4d3d93189713db6303df73e3bda359
Kiki Rizki Amelia <79000861+Kikirizki01@users.noreply.github.com>
2023-01-24 21:43:55 +0700

    Update posts.js

--- models/posts.js
+++ models/posts.js
@@ -3,8 +3,17 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     title: String,
     title2: String,
+    title3: String,
+    title4: String,
+    title5: String,
+    title6: String,
     category: String,
     content: String,
+    content2: String,
+    content3: String,
+    content4: String,
+    content5: String,
+    content6: String,
     image: String,
     created: {
         type: Date,
